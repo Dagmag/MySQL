b85b7cec772776494df3ea9736bd1a4493fdec9a
Dagger <dagger_82@mail.ru>
2023-04-12 21:47:03 +0200

    Lesson5

---
+++ GB_MySQL/MySQL_Lesson5/MySQL_Lesson5.sql
@@ -0,0 +1,43 @@
+USE Lesson5
+
+/*
+1. Создайте представление, в которое попадет информация о пользователях (имя, фамилия, город и пол), которые не старше 20 лет.
+*/
+
+CREATE OR REPLACE VIEW view_user AS 
+SELECT CONCAT(firstname, ' ', lastname, '; ', hometown, '; ', gender) AS 'Пользователи (младше 20 лет)'
+FROM users u
+JOIN profiles p ON u.id = p.user_id
+WHERE TIMESTAMPDIFF(YEAR, birthday, NOW()) < 20
+GROUP BY u.id
+;
+
+SELECT * FROM view_user;
+-- DROP VIEW view_user;
+
+
+/*
+2. Найдите кол-во, отправленных сообщений каждым пользователем и выведите ранжированный список пользователь, 
+указав имя и фамилию пользователя, количество отправленных сообщений и место в рейтинге 
+(первое место у пользователя с максимальным количеством сообщений) . (используйте DENSE_RANK)
+*/
+
+SELECT 
+	DENSE_RANK() OVER (ORDER BY COUNT(from_user_id) DESC) AS 'Место в рейтинге',
+	COUNT(from_user_id) AS 'Количество отправленных сообщений',
+	CONCAT(firstname, ' ', lastname) AS 'Пользователи'
+FROM users u
+JOIN messages m ON u.id = m.from_user_id
+GROUP BY u.id
+;
+
+
+/*
+3. Выберите все сообщения, отсортируйте сообщения по возрастанию даты отправления (created_at) 
+и найдите разницу дат отправления между соседними сообщениями, получившегося списка. (используйте LEAD или LAG)
+*/
+
+SELECT *, 
+LAG(created_at, 1, 0) OVER (PARTITION BY TIMESTAMPDIFF(SECOND, created_at, created_at)) AS lag_created_at, -- смещение на 1 и вместо NULL будет 0
+LEAD(created_at) OVER (PARTITION BY TIMESTAMPDIFF(SECOND, created_at, created_at)) AS lead_created_at
+FROM messages ORDER BY TIMESTAMPDIFF(SECOND, created_at, NOW()) DESC;
